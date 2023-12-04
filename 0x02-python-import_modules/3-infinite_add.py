0b822a4d5effb2d5c5b29f688abce4afdda6da39
M0hamed-0 <moahmedaharoun9@gmail.com>
2023-12-04 03:49:54 +0200

    ayo wtf

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import os
+
+    args = sys.argv
+    argsCount = len(args)
+    fileName = os.path.basename(__file__)
+    file_path = f"./{fileName}"
+    i = 0
+    summ = 0
+    
+    if argsCount == 1:
+        print("0")
+    elif argsCount == 2:
+        summ = args[0] = args[1]
+        print("{}".format(summ))
+    else:
+        while i < argsCount - 1:
+            if args == file_path:
+                continue
+            i += 1
+            summ += int(args[i])
+        print("{}".format(summ))
+
