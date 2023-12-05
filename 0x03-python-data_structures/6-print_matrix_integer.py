0fca0fb7748a3d510a7a16747dfe5ddb9dbce3a8
M0hamed-0 <moahmedaharoun9@gmail.com>
2023-12-05 10:45:19 +0200

    ayo wtf

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     x = 0
-    while x < len(matrix[0]):
+    while x < len(matrix):
         y = 0
-        while y < len(matrix):
+        while y < len(matrix[x]):
             if y != 0:
                 print(" ", end='')
             print("{:d}".format(matrix[x][y]), end='')
