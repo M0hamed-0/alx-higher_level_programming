15614f1502a01124b02f858542deb9197cbb744c
M0hamed-0 <moahmedaharoun9@gmail.com>
2023-12-05 03:00:48 +0200

    ayo wtf

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     mystring = list(my_string)
-    for str in my_string:
+    for str in my_string[:]:
         if str == 'c' or str == "C":
             mystring.remove(str)
     mystring = ''.join(mystring)
