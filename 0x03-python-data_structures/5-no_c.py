2e8100444e094ccb404cfed80db13dfe914f704a
M0hamed-0 <moahmedaharoun9@gmail.com>
2023-12-05 02:56:31 +0200

    ayo wtf

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    mystring = list(my_string)
+    for str in my_string:
+        if str == 'c' or str == "C":
+            mystring.remove(str)
+    mystring = ''.join(mystring)            
+    return (mystring)
